f4ff1e00c4d309f8ab05cc15223e280391593928
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-09-22 17:01:13 +0300

    commit

--- homework_project/settings.py
+++ homework_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hw1_app',
+    'hw1_app.apps.Hw1AppConfig',
 ]
 
 MIDDLEWARE = [
