241009d82f71a9495141f59c5d0d2c5cc71f87d7
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-10-03 17:59:13 +0300

    редакция_5 добавила другие команды для работы с БД магазина

--- hw2_app/management/commands/find_customer.py
+++ hw2_app/management/commands/find_customer.py
@@ -3,14 +3,12 @@ from hw2_app.models import Customer
 
 
 class Command(BaseCommand):
-    help = "Delete customer by ID"
+    help = "Find customer by ID"
 
     def add_arguments(self, parser):
-        parser.add_argument('pk', type=int, help='Customer ID')
+        parser.add_argument('pk', type=int, help='Find customer by ID')
 
     def handle(self, *args, **kwargs):
         pk = kwargs.get('pk')
         client = Customer.objects.filter(pk=pk).first()
-        client.delete()
         self.stdout.write(f'{client}')
-        
--- hw2_app/management/commands/find_product.py
+++ hw2_app/management/commands/find_product.py
@@ -3,13 +3,12 @@ from hw2_app.models import Product
 
 
 class Command(BaseCommand):
-    help = "Delete product by ID"
+    help = "Find product by ID"
 
     def add_arguments(self, parser):
-        parser.add_argument('pk', type=int, help='Product ID')
+        parser.add_argument('pk', type=int, help='Get product by ID')
 
     def handle(self, *args, **kwargs):
         pk = kwargs.get('pk')
         product = Product.objects.filter(pk=pk).first()
-        product.delete()
         self.stdout.write(f'{product}')
